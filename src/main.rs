68b125f9ed0efabe86415b97fba3c72dfce25479
fa-sharp <farshadkt@gmail.com>
2025-09-28 02:35:38 -0400

    PONG

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,33 @@
-#![allow(unused_imports)]
-use std::net::TcpListener;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt, BufWriter},
+    net::{TcpListener, TcpStream},
+};
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
+    println!("tiniredis listening on port 6379...");
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+    loop {
+        match listener.accept().await {
+            Ok((stream, _)) => {
+                tokio::spawn(process(stream));
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+            Err(e) => eprintln!("Error connecting to client: {e}"),
         }
     }
+}
+
+async fn process(socket: TcpStream) -> anyhow::Result<()> {
+    let mut connection = BufWriter::new(socket);
+
+    let c = connection.read_u8().await?;
+    println!("Received '{c}'");
+
+    if let Err(e) = connection.write_all(b"+PONG\r\n").await {
+        eprintln!("Error sending response: {e}")
+    }
+    connection.flush().await?;
+
+    Ok(())
 }
