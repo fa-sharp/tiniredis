2fe6efa26d00fd0503a04ea207517342d3dc9e19
fa-sharp <farshadkt@gmail.com>
2025-10-04 19:41:19 -0400

    use crc-fast crate

--- src/rdb/crc_reader.rs
+++ src/rdb/crc_reader.rs
@@ -1,28 +1,28 @@
-use std::io::{Read, Write};
+use std::io::Read;
 
-use crc64::Crc64;
+use crc_fast::{CrcAlgorithm, Digest};
 
 /// Reader wrapper that calculates CRC64 checksum as it reads
 pub struct Crc64Reader<R> {
     reader: R,
-    checksum: Crc64,
+    checksum: crc_fast::Digest,
 }
 
 impl<R> Crc64Reader<R> {
     pub fn new(reader: R) -> Self {
-        let checksum = Crc64::new();
+        let checksum = Digest::new(CrcAlgorithm::Crc64Redis);
         Self { reader, checksum }
     }
 
     pub fn checksum(&self) -> u64 {
-        self.checksum.get()
+        self.checksum.finalize()
     }
 }
 
 impl<R: Read> Read for Crc64Reader<R> {
     fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
         let n = self.reader.read(buf)?;
-        self.checksum.write_all(&buf[..n])?;
+        self.checksum.update(&buf[..n]);
         Ok(n)
     }
 }
